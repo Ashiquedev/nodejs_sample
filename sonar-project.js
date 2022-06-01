90b31b922a758235f6c81b63d7c3defefc73bf65
Ashiquedev <97375276+Ashiquedev@users.noreply.github.com>
2022-06-01 14:47:52 +0530

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -2,12 +2,12 @@ const sonarqubeScanner = require("sonarqube-scanner");
 sonarqubeScanner({
   serverUrl: 'http://10.1.1.95:9000/',
   options:{
-      sonar.projectKey: 'nodejs'
-      sonar.projectName: 'nodejs'
-      sonar.projectVersion: 'SNAPSHOT-1.0'
-      sonar.login: '3b150f577e7e0b247fa1cf485b3f9a2c22624261'
-      sonar.sources=.
-      sonar.javascript.lcov.reportPaths: coverage/lcov.info
-      sonar.sourceEncoding: UTF-8
+      'sonar.projectKey': 'nodejs',
+      'sonar.projectName': 'nodejs',
+      'sonar.projectVersion': 'SNAPSHOT-1.0',
+      'sonar.login': '3b150f577e7e0b247fa1cf485b3f9a2c22624261',
+      'sonar.sources': '.',
+      'sonar.javascript.lcov.reportPaths': 'coverage/lcov.info',
+      'sonar.sourceEncoding': 'UTF-8'
 }
  },() => {});
